5611a7c25d8f5125d9d30f3d4d74bec05d0935bf
erickscodes <157196665+erickscodes@users.noreply.github.com>
2024-10-12 21:27:47 -0500

    implement signout button on navbar and remove from home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,12 @@ import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { navItems } from "@/constants";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 const Navbar = () => {
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const { status } = useSession();
+  const router = useRouter();
   const toggleNavbar = () => {
     setMobileDrawerOpen(!mobileDrawerOpen);
   };
@@ -25,18 +29,34 @@ const Navbar = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center space-x-4 items-center">
-            <Link
-              href="login"
-              className="py-2 px-3 border rounded-md font-semibold"
-            >
-              Log In
-            </Link>
-            <Link
-              href="register"
-              className="bg-sky-500 py-2 px-3 rounded-md font-semibold"
-            >
-              Sign Up
-            </Link>
+            {status === "authenticated" ? (
+              <Link
+                href=""
+                className="py-2 px-3 border rounded-md font-semibold"
+                onClick={() => {
+                  signOut({ redirect: false }).then(() => {
+                    router.push("/");
+                  });
+                }}
+              >
+                Logout
+              </Link>
+            ) : (
+              <>
+                <Link
+                  href="login"
+                  className="py-2 px-3 border rounded-md font-semibold"
+                >
+                  Log In
+                </Link>
+                <Link
+                  href="register"
+                  className="py-2 px-3 rounded-md bg-sky-500 font-semibold"
+                >
+                  Sign Up
+                </Link>
+              </>
+            )}
           </div>
           <div className="lg:hidden md:flex flex-col justify-end">
             <button onClick={toggleNavbar}>
@@ -54,18 +74,34 @@ const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6 mt-2">
-              <Link
-                href="login"
-                className="py-2 px-3 border rounded-md font-semibold"
-              >
-                Log In
-              </Link>
-              <Link
-                href="register"
-                className="py-2 px-3 rounded-md bg-sky-500 font-semibold"
-              >
-                Sign Up
-              </Link>
+              {status === "authenticated" ? (
+                <Link
+                  href=""
+                  className="py-2 px-3 border rounded-md font-semibold"
+                  onClick={() => {
+                    signOut({ redirect: false }).then(() => {
+                      router.push("/");
+                    });
+                  }}
+                >
+                  Logout
+                </Link>
+              ) : (
+                <>
+                  <Link
+                    href="login"
+                    className="py-2 px-3 border rounded-md font-semibold"
+                  >
+                    Log In
+                  </Link>
+                  <Link
+                    href="register"
+                    className="py-2 px-3 rounded-md bg-sky-500 font-semibold"
+                  >
+                    Sign Up
+                  </Link>
+                </>
+              )}
             </div>
           </div>
         )}
